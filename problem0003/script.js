3eb540aeebcfbadd746b6d14584fb0ca41d64521
Nathan Culley <nathan@nathanculley.com>
2022-09-23 15:36:32 -0400

    Determine prime factors and add to array
    The current state of the project is able to take a dividend and make an array of its prime factors, but the completeness check needs work as it doesn't recognize when a prime factor is the largest for a given divident. The while loop in the cycleDivisors function is commented out while this gets fixed.

--- problem0003/script.js
+++ problem0003/script.js
@@ -12,7 +12,7 @@ function checkDivisibility(dividend) {
 }*/
 
 //let dividend = 600851475143;
-let dividend = 600850
+/*let dividend = 600850
 
 function findLPF(dividend) {
     let lowestFactor = 2;
@@ -27,4 +27,106 @@ function findLPF(dividend) {
     }
     console.log(lowestFactor);
     console.log(largestPrimeFactor);
+}*/
+
+let dividend = 9;
+//prime factors are 3, 5, 7, and 11
+let largestFactorFound = false;
+const factorArray = [];
+
+
+function findLowestPrimeFactor(dividend) {
+
+    if (checkEven(dividend) == true) {
+        factorArray[0] = 2;
+        //console.log(factorArray);
+    }
+    let largestPrimeFactor = cycleDivisors(dividend);
+    console.log(largestPrimeFactor);
+}
+
+function checkEven(dividend) {
+    if (dividend % 2 == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function cycleDivisors(dividend) {
+    let divisor = 3;
+    //console.log(divisor);
+    const primeFactorArray = [];
+    //console.log(factorArray);
+    //console.log(primeFactorArray);
+    //console.log(largestFactorFound);
+    //while (largestFactorFound == false) {
+        let divisible = checkDivisibility(dividend,divisor);
+        //console.log(divisible);
+        if (divisible == true) {
+            //console.log(checkDivisibility(dividend,divisor));
+            factorArray.push(divisor);
+            //console.log(factorArray);
+            //console.log(factorArray.length);
+            for (let i = 0; i < factorArray.length; i++) {
+                //console.log(factorArray.length);
+                //console.log(factorArray[i]);
+                //console.log(checkPrimeness(factorArray[i]));
+                if (checkPrimeness(factorArray[i]) == true) {
+                    //console.log(factorArray[i]);
+                    //console.log(factorArray[i]);
+                    //console.log(checkPrimeness(factorArray[i]));
+                    primeFactorArray.push(factorArray[i]);
+                    //console.log(primeFactorArray);
+                    //console.log(primeFactorArray[i]);
+                }
+            }
+        }
+        console.log(primeFactorArray);
+        console.log(checkCompleteness(primeFactorArray, dividend));
+        if (checkCompleteness(primeFactorArray, dividend) == true) {
+            largestFactorFound = true;
+        }
+        divisor += 2;
+        //console.log(largestFactorFound);
+    //}
+    let largestPrimeFactor = primeFactorArray[primeFactorArray.length - 1];
+    return largestPrimeFactor;
+}
+
+function checkPrimeness(divisor) {
+    //console.log(divisor);
+    for (let i = 2; i < divisor; i ++) {
+        if (divisor % i == 0) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+}
+
+function checkDivisibility(dividend,divisor) {
+    if (dividend % divisor == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function checkCompleteness(primeFactorArray, dividend) {
+    let multiple = 1;
+    for (let i = 0; i < primeFactorArray.length; i++) {
+        multiple *= primeFactorArray[i];
+    }
+    console.log(multiple);
+    console.log(dividend);
+    if (multiple == dividend) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
